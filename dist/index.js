44d3134d4ba3e26671a3a047c4313865ed011a02
Khalyomede <khalyomede@gmail.com>
2020-08-27 12:46:09 +0200

    switched to supporting more browsers (ES3)

--- dist/index.js
+++ dist/index.js
@@ -30,6 +30,34 @@ var geoApiGouvAdresse = (function (exports, axios) {
         });
     }
 
+    function __generator(thisArg, body) {
+        var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+        return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+        function verb(n) { return function (v) { return step([n, v]); }; }
+        function step(op) {
+            if (f) throw new TypeError("Generator is already executing.");
+            while (_) try {
+                if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+                if (y = 0, t) op = [op[0] & 2, t.value];
+                switch (op[0]) {
+                    case 0: case 1: t = op; break;
+                    case 4: _.label++; return { value: op[1], done: false };
+                    case 5: _.label++; y = op[1]; op = [0]; continue;
+                    case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                    default:
+                        if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                        if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                        if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                        if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                        if (t[2]) _.ops.pop();
+                        _.trys.pop(); continue;
+                }
+                op = body.call(thisArg, _);
+            } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+            if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+        }
+    }
+
     /**
      * Execute a search on the French address database and get a list of results.
      *
@@ -49,18 +77,26 @@ var geoApiGouvAdresse = (function (exports, axios) {
      * @throws {TypeError} If the citycode key of the options is not a String.
      * @throws {Error} If no response could be fetched from the server.
      */
-    const search = (searchTerm, options) => __awaiter(void 0, void 0, void 0, function* () {
-        if (typeof searchTerm !== "string") {
-            throw new TypeError("expected argument 1 to be a string");
-        }
-        checkSearchOptions(options);
-        const url = getSearchUrl(searchTerm, options);
-        const response = yield axios__default['default'].get(url);
-        if (response.status !== 200) {
-            throw new Error(`failed to get the results with message ${response.statusText} (code: ${response.status})`);
-        }
-        return response.data;
-    });
+    var search = function (searchTerm, options) { return __awaiter(void 0, void 0, void 0, function () {
+        var url, response;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    if (typeof searchTerm !== "string") {
+                        throw new TypeError("expected argument 1 to be a string");
+                    }
+                    checkSearchOptions(options);
+                    url = getSearchUrl(searchTerm, options);
+                    return [4 /*yield*/, axios__default['default'].get(url)];
+                case 1:
+                    response = _a.sent();
+                    if (response.status !== 200) {
+                        throw new Error("failed to get the results with message " + response.statusText + " (code: " + response.status + ")");
+                    }
+                    return [2 /*return*/, response.data];
+            }
+        });
+    }); };
     /**
      * May throw exceptions if the options are malformed.
      *
@@ -77,8 +113,8 @@ var geoApiGouvAdresse = (function (exports, axios) {
      * @throws {TypeError} If the postcode key of the options is not a String.
      * @throws {TypeError} If the citycode key of the options is not a String.
      */
-    const checkSearchOptions = (options) => {
-        const allowedKeys = [
+    var checkSearchOptions = function (options) {
+        var allowedKeys = [
             "limit",
             "autocomplete",
             "longitude",
@@ -87,7 +123,7 @@ var geoApiGouvAdresse = (function (exports, axios) {
             "postcode",
             "citycode",
         ];
-        const allowedTypes = [
+        var allowedTypes = [
             "street",
             "house",
             "locality",
@@ -101,11 +137,11 @@ var geoApiGouvAdresse = (function (exports, axios) {
         if (!(options instanceof Object)) {
             throw new TypeError("expected argument 2 to be an Object");
         }
-        const unknownKeys = Object.keys(options).filter((key) => !allowedKeys.includes(key));
+        var unknownKeys = Object.keys(options).filter(function (key) { return !allowedKeys.includes(key); });
         if (unknownKeys.length > 0) {
-            const unknownKeyNames = unknownKeys.join(", ");
-            const allowedKeyNames = allowedKeys.join(", ");
-            throw new TypeError(`unknown option ${unknownKeyNames} (allowed: ${allowedKeyNames})`);
+            var unknownKeyNames = unknownKeys.join(", ");
+            var allowedKeyNames = allowedKeys.join(", ");
+            throw new TypeError("unknown option " + unknownKeyNames + " (allowed: " + allowedKeyNames + ")");
         }
         if ("limit" in options) {
             if (typeof options.limit !== "number") {
@@ -140,8 +176,8 @@ var geoApiGouvAdresse = (function (exports, axios) {
                 throw new TypeError("expected key options.type to be a String");
             }
             if (!allowedTypes.includes(options.type)) {
-                const allowedTypeNames = allowedTypes.join(", ");
-                throw new Error(`unknown value ${options.type} for key options.type (allowed: ${allowedTypeNames})`);
+                var allowedTypeNames = allowedTypes.join(", ");
+                throw new Error("unknown value " + options.type + " for key options.type (allowed: " + allowedTypeNames + ")");
             }
         }
         if ("postcode" in options && typeof options.postcode !== "string") {
@@ -158,8 +194,8 @@ var geoApiGouvAdresse = (function (exports, axios) {
      * @param {Object} options The options to customize the search results.
      * @return {String} The constructed URL;
      */
-    const getSearchUrl = (searchTerm, options) => {
-        const url = new URL("https://api-adresse.data.gouv.fr/search/");
+    var getSearchUrl = function (searchTerm, options) {
+        var url = new URL("https://api-adresse.data.gouv.fr/search/");
         url.searchParams.append("q", searchTerm);
         if (options instanceof Object) {
             if ("autocomplete" in options &&
